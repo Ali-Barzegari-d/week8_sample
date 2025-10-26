2b38ec39e288beda8ecb0234ab11804a1aacf3f6
Ali-Barzegari-d <ali.barzegari.1401@gmail.co>
2025-10-27 00:17:44 +0330

    change result question

--- include/Result.hpp
+++ include/Result.hpp
@@ -9,28 +9,41 @@ template <class T, class E>
 class Result {
 public:
     // Construct success value
-    static Result<T, E> Ok(const T& value);
+    static Result<T, E> Ok(const T& value) {
+        // TODO: Create a Result holding a success value (store `value` in `data_`)
+    }
 
     // Construct error value
-    static Result<T, E> Err(const E& error);
+    static Result<T, E> Err(const E& error) {
+        // TODO: Create a Result holding an error value (store `error` in `data_`)
+    }
 
     // Check whether the result is success or error
-    bool is_ok() const noexcept;
-    bool is_err() const noexcept;
+    bool is_ok() const noexcept {
+        // TODO: Return true if `data_` currently holds a success value of type T
+    }
+
+    bool is_err() const noexcept {
+        // TODO: Return true if `data_` currently holds an error value of type E
+    }
 
     // Get success value; throw if this holds an error
-    T unwrap() const;
+    T unwrap() const {
+        // TODO: Return the success value; throw std::runtime_error if this is an error
+    }
 
     // Get error value; throw if this holds success
-    E unwrap_err() const;
+    E unwrap_err() const {
+        // TODO: Return the error value; throw std::runtime_error if this is a success
+    }
 
 private:
-    std::variant<T, E> data_;
+    std::variant<T, E> data_;  // Holds either success or error value
 
     // Private constructor used by factory functions
-    explicit Result(const std::variant<T, E>& v) : data_(v) {}
-
-    // TODO: You may add helper methods if needed.
+    explicit Result(const std::variant<T, E>& v) : data_(v) {
+        // TODO: Store the variant in `data_`
+    }
 };
 
 #endif // RESULT_HPP
