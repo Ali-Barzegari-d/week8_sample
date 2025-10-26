052f3f94a61e9ac1af7627c23de0af4d72621872
Ali-Barzegari-d <ali.barzegari.1401@gmail.co>
2025-10-27 00:02:04 +0330

    fix sol for result

--- include/Result.hpp
+++ include/Result.hpp
@@ -4,23 +4,60 @@
 #include <variant>
 #include <stdexcept>
 #include <string>
+#include <type_traits>
+
+// Struct tags برای تفکیک T و E حتی وقتی یکسان هستن
+struct OkTag {};
+struct ErrTag {};
 
 template <class T, class E>
 class Result {
 public:
-    static Result<T, E> Ok(const T& value);
-    static Result<T, E> Err(const E& error);
+    // Factory methods
+    static Result<T, E> Ok(const T& value) {
+        return Result<T, E>(OkTag{}, value);
+    }
 
-    bool is_ok() const noexcept;
-    bool is_err() const noexcept;
+    static Result<T, E> Err(const E& error) {
+        return Result<T, E>(ErrTag{}, error);
+    }
 
-    T unwrap() const;
-    E unwrap_err() const;
+    // State checkers
+    bool is_ok() const noexcept {
+        return std::holds_alternative<OkWrapper>(data_);
+    }
+
+    bool is_err() const noexcept {
+        return std::holds_alternative<ErrWrapper>(data_);
+    }
+
+    // Accessors
+    T unwrap() const {
+        if (is_ok()) {
+            return std::get<OkWrapper>(data_).value;
+        } else {
+            throw std::runtime_error("Tried to unwrap an error result");
+        }
+    }
+
+    E unwrap_err() const {
+        if (is_err()) {
+            return std::get<ErrWrapper>(data_).value;
+        } else {
+            throw std::runtime_error("Tried to unwrap an ok result");
+        }
+    }
 
 private:
-    std::variant<T, E> data_;
+    // Wrapper برای تفکیک T و E
+    struct OkWrapper { T value; };
+    struct ErrWrapper { E value; };
 
-    explicit Result(const std::variant<T, E>& v) : data_(v) {}
+    std::variant<OkWrapper, ErrWrapper> data_;
+
+    // Constructors
+    Result(OkTag, const T& value) : data_(OkWrapper{value}) {}
+    Result(ErrTag, const E& value) : data_(ErrWrapper{value}) {}
 };
 
 #endif // RESULT_HPP
